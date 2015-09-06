ea452b54ca2bd1308900afe6499e008c4e186f9e
sowiecki <sowiecki@walmartlabs.com>
2015-09-06 12:50:04 -0500

    Order priority of damaged structures.

--- src/queries/damaged-structures.js
+++ src/queries/damaged-structures.js
@@ -1,14 +1,39 @@
-import { rooms, wallHealth } from '../config';
+import { rooms, wallHealth, roadHealth } from '../config';
 
 export default () => {
   for (let room in rooms) {
     Memory.rooms[room].structuresNeedingRepair.length = 0;
+    let stores = [];
+    let roads = [];
+    let walls = [];
+    let others = [];
+
     Game.rooms[room].find(FIND_STRUCTURES, {
       filter: (structure) => {
-        if (structure.hitsMax >= 1 && structure.hits < wallHealth) {
-          Memory.rooms[room].structuresNeedingRepair.push(structure.id);
+        switch (structure.structureType) {
+          case 'road':
+            if (structure.hits < roadHealth) {
+              walls.push(structure.id);
+            }
+          break;
+          case 'constructedWall':
+            if (structure.hits < wallHealth) {
+              walls.push(structure.id);
+            }
+          break;
+          case 'extension':
+          case 'storage':
+          case 'link':
+            if (structure.hits < structure.hitsMax) {
+              walls.push(structure.id);
+            }
+          break;
+          default:
+            others.push(structure.id);
         }
       }
     });
+
+    Memory.rooms[room].structuresNeedingRepair = stores.concat(roads, walls, others);
   }
 };
